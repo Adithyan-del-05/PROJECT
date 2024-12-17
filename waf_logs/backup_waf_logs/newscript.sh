84bb709d3f03af912ffe3770b01f0aa4ca1721eb
Adithyan-del-05 <pradithyan05@gmail.com>
2024-12-17 15:16:20 +0530

    aara

---
+++ waf_logs/backup_waf_logs/newscript.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+# Commands used for this project:
+
+- cd ADHI 
+- ls
+- pwd
+- cd ADHI
+- ls
+- cd cloudcomputing_project
+- ls
+- cd waf_logs
+- ls
+- cd december
+- ls
+- cd ..
+- mv ./January/* ./February/
+- ls
+- mv./log_day/* ./log_day_final/
+- cd February
+- mv ./log_day_1.txt/* ./log_day_1_final.txt/
+- mv log_day_1.txt log_day_1_final.txt
+- mv log_day1.txt log_day_1_final.txt
+- cd ..
+- ls -l January February March April May June July August September October November December
+- touch waf_config.txt
+- ls
+- chmod 770 waf_config.txt
+- ls -l
+- mkdir Backup
+- cd Backup
+- rmdir Backup
+- cd ..
+- ls
+- rmdir Backup
+- mkdir backup_waf_logs
+- cp log_day_1.txt /path/to/backup_waf_logs/
+- cp log_day_1.txt /backup_waf_logs/
+- cp log_day_1.txt backup_waf_logs
+- cd backup_waf_logs/
+- cd ..
+- mkdir backup
+- cd backup
+- log_day_1.txt
+- touch log_day_1.txt
+- ls
+- cd ..
+- cp backup backup_way_logs
+- cp January/ backup_waf_logs
+- cp January backup_waf_logs
+- cp -r January backup_waf_logs
+- cp -r February backup_waf_logs
+- cp -r March backup_waf_logs
+- cp -r April backup_waf_logs
+- cp -r May backup_waf_logs
+- cp -r June backup_waf_logs
+- cp -r July backup_waf_logs
+- cp -r August backup_waf_logs
+- cp -r September backup_waf_logs
+- cp -r October backup_waf_logs
+- cp -r November backup_waf_logs
+- cp -r December backup_waf_logs
+- cd backup_waf_logs
+- history
+- touch script.sh
+- nano script.sh
+- chmod 777 script.sh
+- ./script.sh
+- ls
+- history 20
+- history
+- ping netflix.com
+- ping amazon.com
+- find . -name log.txt_12
+- find . -name log.txt_15
+- uptime
+- sort log.txt_13
+- sort script.sh
+- Tar -cvf <.tar>script.sh
+
+# PURPOSE OF THIS PROJECT:
+
+Server management for a Web Application Firewall (WAF) plays a critical role in ensuring that the WAF operates effectively to protect web applications from various types of cyber threats. The purpose of server management in the context of a WAF includes the following key objectives:
+
+
---
+++ waf_logs/backup_waf_logs/script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+touch log.txt_{1..100}
